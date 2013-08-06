ddc7cb205f7e7b11d99e567cf3e42fa141cef21d
re5et <re5etsmyth@gmail.com>
2013-08-05 17:03:15 -0700

    Add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'restore'
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :progress
+
+  config.fail_fast = true
+
+  config.order = :rand
+end
